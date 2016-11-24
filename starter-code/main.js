453488b042d136d66cd55813109aeb68f0941861
Elliot Mist <zcusimano@gmail.com>
2016-11-24 07:53:49 -0800

    Javascript HTML elements

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,12 +4,23 @@ var cardThree = "king";
 var cardFour = "king";
 
 
-if (cardTwo === cardFour) {
+/*if (cardTwo === cardFour) {
 	alert("Sorry, try again.");
 } else if (cardOne === cardTwo) {
 	alert("You found a match!");
 } else if (cardThree === cardFour) {
 	alert("You found a match!")
+}*/
+
+var createCards = function() {
+
+var gameBoard = document.getElementById('game-board');
+
+    for (i=0; i<4; i++) {
+        var cardDiv = document.createElement('div');
+        cardDiv.className = 'card';
+        gameBoard.appendChild(cardDiv);
+    }
 }
 
-
+createCards();
